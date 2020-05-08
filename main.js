bb38c4abc4267cd1bfd0753b9e20abf80e0755e0
Marcin Krygier <krygier.mar@gmail.com>
2020-05-08 09:56:51 +0200

    navigation changes

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const name="Marcin";
-const age=34;
-const pies="Volt";
-console.log(`Dzień dobry. Nazywam się ${name}. Mam ${age} lata i psa o imieniu ${pies}.`);
+const name = "Marcin";
+const age = 34;
+const pies = "Volt";
+console.log(
+  `Dzień dobry. Nazywam się ${name}. Mam ${age} lata i psa o imieniu ${pies}.`
+);
+
+const heading = document.querySelector('.main-heading--js');
+console.log(heading);
