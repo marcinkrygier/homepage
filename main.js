4e2eafe7e29919d75d5b3fcff744ac0c54bcc326
Marcin Krygier <krygier.mar@gmail.com>
2020-05-22 13:07:55 +0200

    new form & screenresolutions setup

--- main.js
+++ main.js
@@ -2,46 +2,121 @@ const name = "Marcin";
 const age = 34;
 const pies = "Volt";
 
-
-
-
-
-
-
-function calculateOld(myNumber){
+function calculateOld(myNumber) {
   console.log(`Dostalem ${myNumber}`);
-  return myNumber*3;
+  return myNumber * 3;
 }
-const myResult=calculateOld(2);
+const myResult = calculateOld(2);
 console.log(myResult);
 
-
-const calculate=(number)=>number*4;
+const calculate = (number) => number * 4;
 
 console.log(calculate(4));
 
-function greetOld (age, name){
-  console.log( `Cześć, nazywam sie ${name} i mam ${age} lata`);
-
+function greetOld(age, name) {
+  console.log(`Cześć, nazywam sie ${name} i mam ${age} lata`);
 }
-greetOld(age,name);
+greetOld(age, name);
 
-function createContent(querySelectorContent, content){
-  const element=document.querySelector(querySelectorContent);
-  element.innerHTML=content;
+function createContent(querySelectorContent, content) {
+  const element = document.querySelector(querySelectorContent);
+  element.innerHTML = content;
 }
 createContent(`.article__description--js`, `CZESC TU JS`);
 
+const greet = (name, age) => {
+  console.log(`Cześć, z tej strony ${name}, który ma ${age} lata`);
+};
+greet(`Marcin`, age);
 
-
-const greet=(name, age)=>{
-  console.log( 
-    `Cześć, z tej strony ${name}, który ma ${age} lata`
-    );
+if (!(`jaasript` == `java`)) {
+  console.log(`tosie wykona`);
 }
-greet (`Marcin`, age);
 
-if (!(`jaasript`==`java`)){
+const humanOne = {
+  name: `Marcin`,
+  age: 34,
+  adress: {
+    street: `Pobiedziska`,
+    city: `Poznan`,
+  },
+};
 
-  console.log(`tosie wykona`)
+const humanTwo = {
+  name: `Tomek`,
+  age: 20,
+  adress: humanOne.adress,
+};
+
+console.log(humanOne);
+console.log(humanTwo);
+
+console.log(typeof 2);
+console.log(typeof `2`);
+
+if (humanOne.age === humanTwo.age) {
+  console.log(`humanOne jest starszy od humanTwo`);
+} else {
+  console.log(`humanOne jest starszy`);
 }
+
+if (humanTwo.age === 30 || humanOne.age) {
+  console.log(`to sie wykona`);
+}
+
+if (!(`javaScript` == `java`)) {
+  console.log(`java to javaScript`);
+}
+
+if (humanOne.age > humanTwo.age) {
+  console.log(`humanOne jest starszy od humanTwo`);
+} else if (humanOne.age === humanTwo.age) {
+  console.log(`sa rowniesnikami`);
+} else {
+  console.log(`humanOne jest starszy od dwojki`);
+}
+
+const myNumber = 1;
+switch (myNumber) {
+  case 7:
+    console.log(`hi 7`);
+    break;
+
+  case 3:
+    console.log(`hi 3`);
+    break;
+  default:
+    console.log(`deafault response`);
+}
+humanOne.age < humanTwo.age
+  ? console.log(`to prawda`)
+  : console.log(`to nie prawda`);
+
+32 < 20 ? console.log(`true`) : console.log(`false`);
+
+const result = 32 < 20 ? true : `ttrtrtr`; //to samo const result = (32 < 20) ? console.log(`true`) : console.log(`false`); console.log(result);
+console.log(result);
+
+const button = document.querySelector(`.action--js`);
+console.log(button);
+
+const myClick = () => {
+  const heading = document.querySelector(".main__heading--js"); // ten i ten zapis nizej sa rownowazne
+  heading.innerHTML = `CZESC, SKAD PRZYBYWASZ?`;
+};
+button.addEventListener(`click`, myClick);
+
+button.addEventListener(`click`, () => {
+  const heading = document.querySelector(".main__heading--js");
+  heading.innerHTML = `WITAJ NA MOJEJ STRONIE PRZYBYSZU`;
+  heading.classList.toggle(`klasa-z-js`);
+  console.log(heading.classList.contains(`main__heading`));
+});
+
+
+
+const hamburger = document.querySelector(`.hamburger--js`);
+hamburger.addEventListener(`click`, () => {
+  const nav = document.querySelector(`.navigation--js`);
+  nav.classList.toggle(`navigation--open`);
+});
